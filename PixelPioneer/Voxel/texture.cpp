4aa9591fe8bf7ebc7a3e86bc92dfecf243927b3c
KSC <ksc1008@naver.com>
2024-03-08 16:29:21 +0900

    updated some codes

--- PixelPioneer/Voxel/texture.cpp
+++ PixelPioneer/Voxel/texture.cpp
@@ -1,1 +1,28 @@
 #include "texture.h"
+
+void VoxelTexture::loadTextures(TexturePackManifest manifest)
+{
+}
+
+void VoxelTexture::createTextureInstance(const int count, const int width, const int height)
+{
+	glGenTextures(1, &glTexId);
+	glBindTexture(GL_TEXTURE_2D_ARRAY, glTexId);
+	glTexImage3D(GL_TEXTURE_2D_ARRAY,
+		0,                // level
+		GL_RGBA,         // Internal format
+		width, height, count, // width,height,depth
+		0,
+		GL_RGBA,          // format
+		GL_UNSIGNED_BYTE, // type
+		0);               // pointer to data
+}
+
+void VoxelTexture::loadTexture(std::string path, const int texId, const int level)
+{
+}
+
+int VoxelTexture::getFileWidth(std::string path)
+{
+	return 0;
+}
--- PixelPioneer/Voxel/texture.h
+++ PixelPioneer/Voxel/texture.h
@@ -2,5 +2,20 @@
 #include"../Graphics/texturepack.h"
 
 class VoxelTexture {
+	int size;
+	GLuint glTexId;
+
+public:
+	VoxelTexture(TexturePackManifest manifest);
+	~VoxelTexture();
+
+	void loadTextures(TexturePackManifest manifest);
+
+private:
+	void createTextureInstance(const int count, const int width, const int height);
+
+	void loadTexture(std::string path, const int texId, const int level);
+
+	int getFileWidth(std::string path);
 
 };
